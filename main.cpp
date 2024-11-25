a465ed830e9296183059fa29a45c747bc08105d6
Kostya <vertogpro@gmail.com>
2024-11-25 17:09:12 +0500

    finish with move semantics

--- main.cpp
+++ main.cpp
@@ -1,13 +1,15 @@
-#include "string.hpp"
-
 #include <iostream>
+#include <utility> // для std::move
+
+#include "string.hpp"
 
 int main() {
     vertoker::string test1{};
     vertoker::string test2{"Hello in ASCII"};
     vertoker::string test3{test1}; // copy null
     vertoker::string test4{test2}; // copy
+    vertoker::string test5{std::move(test2)}; // move
 
-    std::cin.ignore();
+    //std::cin.ignore();
     return 0;
 }
--- string.hpp
+++ string.hpp
@@ -6,50 +6,58 @@
 #include <assert.h>
 #include <stdio.h>
 
+// тупой способ тестирования работоспособности строки через макросы
+#define USE_TEST_LOG 0
+
 namespace vertoker { // ну раз это моя реализация, значит и namespace будет мой
 class string {
+private:
+    #define TEST_LOG(str) USE_TEST_LOG ? printf(str) : 0;
+
 public:
     string() : ptr{nullptr} { 
-        //printf("Null constructor\n");
+        TEST_LOG("Null constructor\n");
     }
     string(const char* str) {
         if (str == nullptr) {
-            //printf("Create constructor null\n");
+            TEST_LOG("Create constructor null\n");
             ptr = nullptr;
         }
         else {
-            //printf("Create constructor\n");
-            size_t length = strlen(str);
-            ptr = (char*)malloc(length + 1);
-            strcpy(ptr, str);
-            ptr[length] = '\0';
+            TEST_LOG("Create constructor\n");
+            copy(str);
         }
     }
 
     string(const string& other) { // copy
         if (other.ptr == nullptr) {
-            //printf("Copy constructor null\n");
+            TEST_LOG("Copy constructor null\n");
             ptr = nullptr;
         }
         else {
-            //printf("Copy constructor\n");
-            size_t length = strlen(other.ptr);
-            ptr = (char*)malloc(length + 1);
-            strcpy(ptr, other.ptr);
-            ptr[length] = '\0';
+            TEST_LOG("Copy constructor\n");
+            copy(other.ptr);
         }
     }
     string(string&& other) { // move
-        //printf("Move constructor\n");
+        TEST_LOG("Move constructor\n");
         ptr = other.ptr;
         other.ptr = nullptr;
     }
 
     ~string() {
+        TEST_LOG("Free deconstructor\n");
         free(ptr);
     }
 
 private:
+    void copy(const char* dst) {
+        size_t length = strlen(dst);
+        ptr = (char*)malloc(length + 1);
+        strcpy(ptr, dst);
+        ptr[length] = '\0';
+    }
+
     char* ptr; // главный указатель
 }; // string
 } // vertoker
