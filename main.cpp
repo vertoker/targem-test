705dcdf99f47cd2bcb57409f823d7b191b84d836
Kostya <vertogpro@gmail.com>
2024-11-25 17:50:21 +0500

    create first + operators

--- main.cpp
+++ main.cpp
@@ -4,11 +4,14 @@
 #include "string.hpp"
 
 int main() {
-    vertoker::string test1{};
-    vertoker::string test2{"Hello in ASCII"};
+    vertoker::string test0{};
+    vertoker::string test1{"test1"};
+    vertoker::string test2{"test2"};
     vertoker::string test3{test1}; // copy null
     vertoker::string test4{test2}; // copy
     vertoker::string test5{std::move(test2)}; // move
+
+    //vertoker::string operator1 = test1 + test2; // test1test2
 
     //std::cin.ignore();
     return 0;
--- string.hpp
+++ string.hpp
@@ -7,18 +7,18 @@
 #include <stdio.h>
 
 // тупой способ тестирования работоспособности строки через макросы
-#define USE_TEST_LOG 0
+#define USE_TEST_LOG 1
 
 namespace vertoker { // ну раз это моя реализация, значит и namespace будет мой
 class string {
 private:
     #define TEST_LOG(str) USE_TEST_LOG ? printf(str) : 0;
 
-public:
+public: // constructors and destructors
     string() : ptr{nullptr} { 
         TEST_LOG("Null constructor\n");
     }
-    string(const char* str) {
+    string(const char* str) { // create
         if (str == nullptr) {
             TEST_LOG("Create constructor null\n");
             ptr = nullptr;
@@ -28,7 +28,6 @@ public:
             copy(str);
         }
     }
-
     string(const string& other) { // copy
         if (other.ptr == nullptr) {
             TEST_LOG("Copy constructor null\n");
@@ -44,17 +43,109 @@ public:
         ptr = other.ptr;
         other.ptr = nullptr;
     }
+    
+    string(const char* str1, const char* str2) { // merge 1
+        if (str1 == nullptr || *str1 == '\0') {
+            TEST_LOG("Operator += other empty\n");
+            return;
+        }
+        else if (str2 == nullptr || *str2 == '\n') {
+            TEST_LOG("Operator += other copy\n");
+            return;
+        }
+        else {
+            TEST_LOG("Operator +=\n");
+            merge(str1, str2);
+        }
+    }
+    string(const string& str1, const string& str2) { // merge 2
+        if (str1.ptr == nullptr || *str1.ptr == '\0') {
+            TEST_LOG("Operator += other empty\n");
+            return;
+        }
+        else if (str2.ptr == nullptr || *str2.ptr == '\n') {
+            TEST_LOG("Operator += other copy\n");
+            return;
+        }
+        else {
+            TEST_LOG("Operator +=\n");
+            merge(str1.ptr, str2.ptr);
+        }
+    }
 
     ~string() {
         TEST_LOG("Free deconstructor\n");
         free(ptr);
     }
 
+public: // operators
+
+    void operator+=(const char* str) {
+        if (str == nullptr || *str == '\0') {
+            TEST_LOG("Operator += other empty\n");
+            return;
+        }
+        else if (ptr == nullptr || *ptr == '\n') {
+            TEST_LOG("Operator += other copy\n");
+            copy(str);
+        }
+        else {
+            TEST_LOG("Operator +=\n");
+            append(str);
+        }
+    }
+    void operator+=(const string& other) {
+        this->operator+=(other.ptr);
+    }
+
+    static string operator+(const char* str1, const char* str2) {
+        string result{str1, str2};
+        return result;
+    }
+    static string operator+(const string& str1, const char* str2) {
+        string result{str1.ptr, str2};
+        return result;
+    }
+    static string operator+(const char* str1, const string& str2) {
+        string result{str1, str2.ptr};
+        return result;
+    }
+    static string operator+(const string& str1, const string& str2) {
+        string result{str1, str2};
+        return result;
+    }
+
+public: // other
+    char* c_ptr() { return ptr; }
+
 private:
     void copy(const char* dst) {
         size_t length = strlen(dst);
         ptr = (char*)malloc(length + 1);
+
         strcpy(ptr, dst);
+        ptr[length] = '\0';
+    }
+    void append(const char* dst) {
+        size_t length1 = strlen(ptr);
+        size_t length = length1 + strlen(dst);
+
+        char* ptr1 = (char*)malloc(length + 1);
+        char* ptr2 = ptr1 + length1;
+
+        strcpy(ptr1, ptr);
+        strcpy(ptr2, dst);
+        ptr1[length] = '\0';
+    }
+    void merge(const char* str1, const char* str2) {
+        size_t length1 = strlen(str1);
+        size_t length = length1 + strlen(str2);
+
+        ptr = (char*)malloc(length + 1);
+        char* ptr2 = ptr + length1;
+
+        strcpy(ptr, str1);
+        strcpy(ptr2, str2);
         ptr[length] = '\0';
     }
 
